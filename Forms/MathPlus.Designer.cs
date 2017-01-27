94bad7bfc654d3530559c248d06be63c25e9f269
yrk-93 <yrk-93@tut.by>
2017-01-27 20:13:09 +0300

    Keys input problem has been solved

--- Forms/MathPlus.Designer.cs
+++ Forms/MathPlus.Designer.cs
@@ -232,6 +232,7 @@
             this.Controls.Add(this.btn1);
             this.Controls.Add(this.btn0);
             this.DoubleBuffered = true;
+            this.KeyPreview = true;
             this.MaximizeBox = false;
             this.Name = "MathPlus";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
