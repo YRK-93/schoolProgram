cf4688643a7f997be8616d527e87f725785dffbe
yrk-93 <yrk-93@tut.by>
2017-01-28 11:42:43 +0300

    MainForm has been refactored

--- Classes/MainView.cs
+++ Classes/MainView.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using SHKOLA.Properties;
 
 namespace SHKOLA
 {
-    class MainView : BaseView
+    public partial class MainForm
     {
-        public static void OnMathClicked()
+        List<WinElement> controlsList;
+
+        public MainForm()
         {
+            InitializeComponent();
+
+            // Initialization
+            controlsList = new List<WinElement>();
+            controlsList.Add(new ButtonControl(ref this.button1, 0.2f, 0.3f, 0.4f, 0.16f, Resources.btnMathG, this));
+        }
+
+        public void Actualize()
+        {
+            if (controlsList != null)
+                foreach (WinElement wElem in controlsList)
+                    wElem.OnUpdate(this);
+        }
+
+        public void OnMathClicked()
+        {
+            this.Hide();
             var mathForm = new MathSelectMode();
             mathForm.ShowDialog();
+            this.Show();
         }
     }
 }
--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -6,40 +6,25 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using SHKOLA.Properties;
 
 namespace SHKOLA
 {
     public partial class MainForm : Form
     {
-        public MainForm()
-        {
-            InitializeComponent();
-            Initilization();
-        }
-
-        public void Initilization()
-        {
-            // buttons settings
-            MainView.AddElement(new ButtonControl(ref this.button1, 0.2f, 0.3f, 0.4f, 0.16f, Resources.btnMathG, this));
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-            MainView.Actualize(this);
+            Actualize();
             this.TopMost = false;
         }
 
         private void Form1_Resize(object sender, EventArgs e)
         {
-            MainView.Actualize(this);
+            Actualize();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.Hide();
-            MainView.OnMathClicked();
-            this.Show();
+            OnMathClicked();
         }
     }
 }
